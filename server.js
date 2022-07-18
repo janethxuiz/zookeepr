9bb650d6fb5e19c7a808b65432ce1f1ebc568a54
janethxuiz <janethruiz152@gmail.com>
2022-07-18 02:59:24 -0700

    updated animal function

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { animals } = require('./data/animals');
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -46,12 +52,21 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query) {
-        results = filterByQuery(req.query, results);
-    }
-    res.json(animals);
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+  
+    return animal;
+  }
+
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+  
+    res.json(animal);
   });
 
   app.get('/api/animals/:id', (req, res) => {
@@ -61,6 +76,13 @@ app.get('/api/animals', (req, res) => {
     } else {
       res.send(404);
     }
+  });
+
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    res.json(req.body);
   });
 
 app.listen(PORT, () => {
